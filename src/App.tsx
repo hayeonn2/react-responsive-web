f0c8c19a1846f56316b2ca9a21c1057867b869f3
hayoenn2 <dwd1205@naver.com>
2024-01-23 23:41:47 +0900

    feat : useMediaQuery 사용해서 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
-import './App.css';
+import { useMediaQuery } from 'react-responsive';
 
 function App() {
-	return <div className="App">react-responsive-web</div>;
+	const isPc = useMediaQuery({ query: '(min-width: 1024px)' });
+	const isTablet = useMediaQuery({
+		query: '(min-width: 768px) and (max-width: 1023px)',
+	});
+	const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+
+	return (
+		<div>
+			{isPc && <p>PC</p>}
+			{isTablet && <p>Tablet</p>}
+			{isMobile && <p>Mobile</p>}
+		</div>
+	);
 }
 
 export default App;

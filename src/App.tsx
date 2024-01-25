3ea4d2f8f7a551a171607e867a5fb3c7dc104c7a
hayoenn2 <dwd1205@naver.com>
2024-01-25 16:50:47 +0900

    feat : hooks 이용 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import { useMediaQuery } from 'react-responsive';
 
 function App() {
 	const isPc = useMediaQuery({ query: '(min-width: 1024px)' });
-	const isTablet = useMediaQuery({
-		query: '(min-width: 768px) and (max-width: 1023px)',
-	});
-	const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+
+	const queryText = isPc ? '웹페이지' : '모바일버전!!';
+	// const isTablet = useMediaQuery({
+	// 	query: '(min-width: 768px) and (max-width: 1023px)',
+	// });
+	// const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
 
 	return (
 		<div>
-			{isPc && <p>PC</p>}
+			{queryText}
+			{/* {isPc && <p>PC</p>}
 			{isTablet && <p>Tablet</p>}
-			{isMobile && <p>Mobile</p>}
+			{isMobile && <p>Mobile</p>} */}
 		</div>
 	);
 }
